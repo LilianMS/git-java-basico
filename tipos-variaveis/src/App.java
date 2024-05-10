c201898bdbbefe21db4bc6601755410f59bb4d59
LilianMS <lm.webdesign02@gmail.com>
2024-05-10 18:44:41 -0300

    iniciando java basico

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá! Hello, World");
+        System.out.println("Hello, World!");
     }
 }

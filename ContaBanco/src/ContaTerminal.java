8bb09901209ad8a11da3dda009178511810ad233
LilianMS <lm.webdesign02@gmail.com>
2024-05-15 23:00:39 -0300

    desafio ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner;
+        String nomeCliente;
+        String agencia;
+        int numero;
+        double saldo;
+
+        scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá! Por favor, digite seu nome: ");
+        nomeCliente = scanner.next();
+        System.out.println("Sr(a). " + nomeCliente + ", Qual o número de sua agência?");
+        agencia = scanner.next();
+        System.out.println("Agora, informe o número de sua conta: ");
+        numero = scanner.nextInt();
+        System.out.println("Digito o valor de depósito: ");
+        saldo = scanner.nextDouble();
+        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
+    }
+}
